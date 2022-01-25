f74df03ef6f3fc712585f62b7aace961799922c5
Kevin Gagnon <kevin.gagnon@broadsign.com>
2022-01-24 23:23:06 -0500

    Add PaperCSS and Filters

--- .eleventy.js
+++ .eleventy.js
@@ -35,6 +35,7 @@ module.exports = function (eleventyConfig) {
 		"./src/admin/css/styles.css": "./admin/css/styles.css",
 		"./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
 		"./node_modules/prismjs/themes/prism-tomorrow.css": "./static/css/prism-tomorrow.css",
+		"./node_modules/papercss/dist/paper.css": "./static/css/paper.css",
 	});
 
 	// Copy Image Folder to /_site

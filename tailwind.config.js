45699503654fc0375eb17bb3bddfd0977b061b2a
Kevin Gagnon <kevin.gagnon@broadsign.com>
2022-01-26 09:15:12 -0500

    Tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 module.exports = {
-  purge: {
-    mode: "all",
-    content: ["./**/*.html"],
-    options: {
-      whitelist: [],
-    },
-  },
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      colors: {},
-    },
-  },
-  variants: {},
-  plugins: [require("@tailwindcss/typography")],
+	purge: {
+		mode: "all",
+		content: ["./**/*.html"],
+		options: {
+			whitelist: [],
+		},
+	},
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			colors: {},
+			lineHeight: {
+				reset: "1",
+			},
+		},
+	},
+	variants: {},
+	plugins: [require("@tailwindcss/typography")],
 };
